d49651b1572db1229bada101cf4f0eb9986917d1
Konstantin Grigoriev <Konstantin.V.Grigoriev@gmail.com>
2014-12-05 01:32:04 +0300

    Client: setting bookcaseId for book implemented.

--- client/app/book/book.controller.js
+++ client/app/book/book.controller.js
@@ -4,15 +4,22 @@ angular.module('scriba.book')
   .controller('BookListCtrl', function ($log, $modal, $routeParams, BookcaseService) {
     var data = this;
 
+    data.bookcaseId = $routeParams.bookcaseId;
+
     function refresh() {
-      data.books = BookcaseService.queryBooks({bookcaseId: $routeParams.bookcaseId});
+      data.books = BookcaseService.queryBooks({bookcaseId: data.bookcaseId});
     }
 
     this.add = function () {
       var modalInstance = $modal.open({
         templateUrl: 'app/book/book.detail.html',
         controller: 'BookDetailCtrl',
-        controllerAs: 'bookDetailCtrl'
+        controllerAs: 'bookDetailCtrl',
+        resolve: {
+          bookcaseId: function () {
+            return data.bookcaseId;
+          }
+        }
       });
 
       modalInstance.result.then(function () {
@@ -22,8 +29,8 @@ angular.module('scriba.book')
 
     refresh();
   })
-  .controller('BookDetailCtrl', function ($modalInstance, BookService) {
-    this.book = {};
+  .controller('BookDetailCtrl', function ($modalInstance, BookService, bookcaseId) {
+    this.book = {bookcase: bookcaseId};
 
     this.save = function (bookForm) {
       if (bookForm.$valid) {
