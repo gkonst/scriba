f96564b1dd59e95c582c8008aa8e8ffb8c1fc790
Konstantin Grigoriev <Konstantin.V.Grigoriev@gmail.com>
2014-12-18 13:01:54 +0300

    Server: ValidationError handling added.

--- server/components/errors/errorhandler.js
+++ server/components/errors/errorhandler.js
@@ -4,16 +4,19 @@ module.exports = function (app) {
   //jshint unused: false
   //noinspection JSUnusedLocalSymbols
   app.use(function (err, req, res, next) {
-    var status = err.status || 500;
-    if (status === 500) {
-      console.error(err.stack);
+    if (err.name === 'ValidationError') {
+      res.status(400).json(err);
+    } else {
+      var status = err.status || 500;
+      if (status === 500) {
+        console.error(err.stack);
+      }
+      res.status(status).send({
+        status: status,
+        message: err.message,
+        code: err.code
+      });
     }
-    res.status(status);
-    res.send({
-      status: status,
-      message: err.message,
-      code: err.code
-    });
   });
 };
 
