01e63cc6bb69a52c0823923fb6e04a0ec6fda2fa
Konstantin Grigoriev <Konstantin.V.Grigoriev@gmail.com>
2014-12-23 22:05:32 +0300

    Server: error handler test improved.

--- server/components/errors/errorhandler.spec.js
+++ server/components/errors/errorhandler.spec.js
@@ -13,13 +13,24 @@ describe('ErrorHandler', function () {
     app = express();
   });
 
-  it('should return error if status is 500', function (done) {
-    // given
-    var error = {stack: 'test stack', message: 'test message', code: 'test code'};
+  function controllerWithError(error) {
     app.get('/', function (req, res, next) {
       next(error);
     });
     errorHandler(app);
+  }
+
+  it('should return error with fixed structure', function (done) {
+    // given
+    var error = {
+      name: 'SomeError',
+      status: 500,
+      stack: 'test stack',
+      message: 'test message',
+      code: 'test code',
+      foo: 'bar'
+    };
+    controllerWithError(error);
 
     // when & then
     request(app)
@@ -31,8 +42,58 @@ describe('ErrorHandler', function () {
         }
         res.body.message.should.equal(error.message);
         res.body.code.should.equal(error.code);
+        res.body.should.not.eql(error);
+        done();
+      });
+  });
+
+  it('should return error with fixed structure and status 500 if status is missed in error', function (done) {
+    // given
+    var error = {
+      name: 'SomeError',
+      stack: 'test stack',
+      message: 'test message',
+      code: 'test code',
+      foo: 'bar'
+    };
+    controllerWithError(error);
+
+    // when & then
+    request(app)
+      .get('/')
+      .expect(500)
+      .end(function (err, res) {
+        if (err) {
+          done(err);
+        }
+        res.body.message.should.equal(error.message);
+        res.body.code.should.equal(error.code);
+        res.body.should.not.eql(error);
+        done();
+      });
+  });
+
+  it('should return error as is and status 400 for validation errors', function (done) {
+    // given
+    var error = {
+      name: 'ValidationError',
+      stack: 'test stack',
+      message: 'test message',
+      code: 'test code',
+      foo: 'bar'
+    };
+    controllerWithError(error);
+
+    // when & then
+    request(app)
+      .get('/')
+      .expect(400)
+      .end(function (err, res) {
+        if (err) {
+          done(err);
+        }
+        res.body.should.eql(error);
         done();
       });
   });
 });
-
